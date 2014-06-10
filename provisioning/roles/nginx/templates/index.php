5c9abe839cec6461e8c02b9968c7354de54935b9
Jean-Christophe Cuvelier <jcc@morris-chapman.com>
2014-06-10 09:43:07 +0200

    First working version of vagrant with symfony

---
+++ provisioning/roles/nginx/templates/index.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>Server installed</title>
+  </head>
+  <body>
+    <h1>Server installed!</h1>
+    <p>Your server has successfully been installed!</p>
+    <hr />
+    <?php echo date('d/m/Y H:i:s'); ?>
+  </body>
+</html>
